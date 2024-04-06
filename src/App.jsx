450479c327a254988f0bc48491a18958c13f242e
Kartik <iamkaranmishra551@gmail.com>
2024-04-06 22:55:16 +0530

    fix some bugs.

--- src/App.jsx
+++ src/App.jsx
@@ -35,13 +35,13 @@ function App() {
   const pwdRef = useRef(null);
   return (
     <>
-      <div className="border mx-auto max-w-[700px]  sm:w-[700px] rounded-xl space-y-3 bg-gray-500 py-6 px-4 my-2 ">
-        <h1 className=" text-4xl sm:text-5xl text-center text-white">
+      <div className=" w-full border mx-auto max-w-md rounded-xl space-y-3 bg-gray-500 py-6 px-4 my-2 ">
+        <h1 className=" text-4xl sm:text-3xl text-center text-white whitespace-nowrap">
           Password Generator
         </h1>
-        <div className="flex items-center space-x-3 sm:justify-center justify-end">
+        <div className="flex items-center space-x-3 sm:justify-center justify-end overflow-hidden">
           <input
-            className="border py-4 px-6 rounded-2xl text-2xl text-orange-500"
+            className="border py-4 px-3 rounded-2xl text-2xl text-orange-500 w-full"
             type="text"
             placeholder="Password"
             readOnly
@@ -52,16 +52,15 @@ function App() {
             type="button"
             className="text-2xl border bg-gradient-to-br from-purple-600 to-blue-500  inline-block py-4 px-6 rounded-xl  text-white shadow-lg hover:text-[#f5f5f5] hover:bg-gradient-to-bl focus:outline-none"
             onClick={() => {
-              copyPassword(),
-                setBtnText('copied');
+              copyPassword(), setBtnText("copied");
             }}
             id="btn"
           >
             {btnText}
           </button>
         </div>
-        <div className="flex justify-center items-center w-[100%] mx-auto text-2xl text-white px-1 sm:px-4 py-2">
-          <div className="flex text-sm sm:text-lg w-1/2 justify-items-end sm:justify-center mx-2">
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
             <input
               type="range"
               name="range"
@@ -70,43 +69,43 @@ function App() {
               value={length}
               min={6}
               max={100}
-              onChange={(event) => {setLength(event.target.value),setBtnText("copy")}}
+              onChange={(event) => {
+                setLength(event.target.value), setBtnText("copy");
+              }}
             />
             <label htmlFor="range" className="pl-1 sm:px-3 whitespace-nowrap">
               Length : {length}
             </label>
           </div>
-          <div className="w-1/2 flex">
-            <div className=" mx-2 flex gap-x-2  text-sm sm:text-lg">
-              <input
-                type="checkbox"
-                name="number"
-                id="check"
-                defaultChecked={numAllowed}
-                onChange={() => {
-                  setNumAllowed((prev) => !prev),setBtnText("copy");
-                }}
-                className="mr-2 cursor-pointer"
-              />
-              <label htmlFor="number" className=" text-white">
-                Number
-              </label>
-            </div>
-            <div className=" mx-2 flex gap-x-2  text-sm sm:text-lg">
-              <input
-                type="checkbox"
-                name="character"
-                id="character"
-                defaultChecked={charAllowed}
-                onChange={() => {
-                  setCharAllowed((prev) => !prev),setBtnText("copy");
-                }}
-                className="mr-2 cursor-pointer"
-              />
-              <label htmlFor="character" className=" text-white">
-                Character
-              </label>
-            </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              name="number"
+              id="check"
+              defaultChecked={numAllowed}
+              onChange={() => {
+                setNumAllowed((prev) => !prev), setBtnText("copy");
+              }}
+              className="mr-2 cursor-pointer"
+            />
+            <label htmlFor="number" className=" text-white">
+              Number
+            </label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              name="character"
+              id="character"
+              defaultChecked={charAllowed}
+              onChange={() => {
+                setCharAllowed((prev) => !prev), setBtnText("copy");
+              }}
+              className="mr-2 cursor-pointer"
+            />
+            <label htmlFor="character" className=" text-white">
+              Character
+            </label>
           </div>
         </div>
       </div>
